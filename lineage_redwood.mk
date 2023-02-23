31c740a6806a4d5d0e774252064fc07fc83f74fa
Steven Marga <steven.marga@gmail.com>
2023-02-23 16:26:17 +0200

    Added lineage make file

---
+++ lineage_redwood.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from redwood device
+# TODO:
+#$(call inherit-product, device/xiaomi/redwood/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := redwood
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 22101320G
+PRODUCT_NAME := lineage_redwood
+
+# TODO
+#PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_SYSTEM_NAME := redwood_global
+PRODUCT_SYSTEM_DEVICE := redwood
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redwood-user 12 SKQ1.220303.001 V14.0.5.0.SMSCNXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Redmi/redwood/redwood:12/RKQ1.211001.001/V14.0.5.0.SMSCNXM:user/release-keys
